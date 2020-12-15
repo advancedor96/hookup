cce8836630cbac3e78b8396afbd71df60ffea378
ding <ding@dingdeMacBook-Pro.local>
2020-12-15 23:50:19 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+
+    <div id="doc" class="markdown-body container-fluid comment-inner comment-enabled" data-hard-breaks="true"><h1 id="高雄人徵求約炮" data-id="高雄人徵求約炮" data-original-title="" title=""><a class="anchor hidden-xs" href="#高雄人徵求約炮" title="高雄人徵求約炮"><span class="octicon octicon-link"></span></a><span>高雄人徵求約炮</span></h1><p><span>本人規格：身高182cm，有肚子，體脂率24%、屌長：勃起後13cm，勃起前很小。粗度：亞洲人尺寸。</span></p><p><span>性經驗：女友2任，現在第2任(開放式關係)。曾在澳洲嫖妓過1次。總結：跟3個人發生過性關係。</span></p><p><span>約炮需求：全程戴套、開房間(男女平分以實現平權)、可溝通、能誠實表達自己感受(舒服 or 不舒服 or 沒意見)。</span></p><p><span>希望對象：長髮(重要)、大隻馬(加分)、變性人也可(變女)</span></p><h2 id="【想法】" data-id="【想法】"><a class="anchor hidden-xs" href="#【想法】" title="【想法】"><span class="octicon octicon-link"></span></a><span>【想法】</span></h2><p><span>之前想約炮很久，也曾公開徵求過，真的有一些人表示有意願。但對方挑我、我自己也挑，所以不了了之。但我覺得沒差，性欲不是必需品，寧缺勿濫。</span></p><p><span>在床上的時候，我很願意幫你服務，按摩什麼的都行。一個好的性行為，是兩人互動創造出來的，我比較拙劣(笨)，但只要你願意講，我就能幫你服務，讓你舒服，但你得願意講。</span></p><p><span>事前，我們能先吃個飯或是喝個飲料，互動一下看看感覺(面試)。如果雙方都有意願再試，再找時間開房間啦。我很能接受"被拒絕"，也希望你能接受被拒絕。</span></p><p><span>事中，全程戴套是基本。若到一半不舒服，也可以隨時喊停。我的性欲能自己處理，不是那種停不下來的人，所以別擔心。</span></p><p><span>事後，我不抽菸，但能聊天(你願意的話)。</span></p><p><span>沒有固炮這回事，歡迎玩咖、或是想玩玩、想約炮看看的人。</span><div><i>本文寫於2020/12/14</i></div><p><span>意者可加LINE id：peanut962012</span><br>
+<span>交換更多照片以及聊聊。(收得到陌生訊息)</span></p><blockquote>
+<p><span>時間：一到五晚上、六日。</span><br>
+<span>地區：高雄市。</span></p>
+</blockquote><p><span>給喜歡 Email 聯絡的人：advancedor103@gmail.com</span><br>
+<span>給喜歡 FB 聯絡的人：</span><a href="https://lihi1.cc/OLW3I" target="_blank" rel="noopener"><span>https://lihi1.cc/OLW3I</span></a></p></div>
+
+  <ShareNetwork
+    network="facebook"
+    url="http://172.104.76.31/hookup/"
+    title="高雄男約炮、誠意約"
+    description="本人規格：身高182cm，有肚子，體脂率24%、屌長：勃起後13cm，勃起前很小。粗度：亞洲人尺寸。性經驗：女友2任，現在第2任(開放式關係)。曾在澳洲嫖妓過1次。總結：跟3個人發生過性關係。"
+    quote="歡迎玩咖、或是想玩玩、想約炮看看的人"
+    class="share"
+  >
+  分享於FB
+</ShareNetwork>
+<ShareNetwork
+    network="Telegram"
+    url="http://172.104.76.31/hookup/"
+    title="高雄男約炮、誠意約"
+    description="本人規格：身高182cm，有肚子，體脂率24%、屌長：勃起後13cm，勃起前很小。粗度：亞洲人尺寸。性經驗：女友2任，現在第2任(開放式關係)。曾在澳洲嫖妓過1次。總結：跟3個人發生過性關係。"
+    quote="歡迎玩咖、或是想玩玩、想約炮看看的人"
+    class="share"
+  >
+    分享於Telegram
+</ShareNetwork>
+
+<ShareNetwork
+    network="Twitter"
+    url="http://172.104.76.31/hookup/"
+    title="高雄男約炮、誠意約"
+    description="本人規格：身高182cm，有肚子，體脂率24%、屌長：勃起後13cm，勃起前很小。粗度：亞洲人尺寸。性經驗：女友2任，現在第2任(開放式關係)。曾在澳洲嫖妓過1次。總結：跟3個人發生過性關係。"
+    quote="歡迎玩咖、或是想玩玩、想約炮看看的人"
+    class="share"
+  >
+    分享於Twitter
+</ShareNetwork>
+
+      <v-img  :src="require('./assets/1.jpg')"></v-img>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { },
+  data: () => {
+    return {
+      show: false
+
+    }
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0 10px;
+}
+.share{
+  margin: 0 50px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+import VueSocialSharing from 'vue-social-sharing'
 
 Vue.config.productionTip = false
+Vue.use(VueSocialSharing)
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
